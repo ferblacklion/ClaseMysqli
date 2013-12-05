27f85e83a214e0180f8232d368de059f6108d983
ferblacklion <mo.frc08@gmail.com>
2013-12-05 09:36:12 -0600

    change var
    cambio de variables define a cont

--- DB.php
+++ DB.php
@@ -1,12 +1,12 @@
 <?php
 
 /* constantes para la conexión */
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'users_pl');
-define('DB_NAME_TEST', 'galloapp');
-define('CHARSET','utf8');
+const DB_HOST      = 'localhost';
+const DB_USER      = 'root';
+const DB_PASS      = '';
+const DB_NAME      = 'users_pl';
+const DB_NAME_TEST = 'galloapp';
+const CHARSET      = 'utf8';
 
 
 
